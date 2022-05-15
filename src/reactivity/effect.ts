8b32272662d0c72e69a7da6c1eaeee9a61e00fe9
chenym <chenym_sh@ushareit.com>
2022-05-15 22:05:22 +0800

    feat effect should run

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -6,7 +6,7 @@ class ReactiveEffect {
   }
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 const targetMap = new Map();
@@ -40,6 +40,7 @@ let activeEffect;
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn);
   _effect.run();
+  return _effect.run.bind(_effect);
 }
 
 
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -16,5 +16,20 @@ describe('effect', () => {
     // 更新
     user.age++;
     expect(nextAge).toBe(12);
-  })
+  });
+
+  it("should return runner when call effect", () => {
+    // 当调用 runner 的时候可以重新执行 effect.run
+    // runner 的返回值就是用户给的 fn 的返回值
+    let foo = 10;
+    const runner = effect(() => {
+      foo++;
+      return foo;
+    });
+
+    expect(foo).toBe(11);
+    runner();
+    expect(foo).toBe(12);
+    expect(runner()).toBe(13);
+  });
 })
