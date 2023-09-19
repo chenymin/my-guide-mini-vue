4c9856f0a95d2ec4caff974bb55ac308ea683113
chenym <chenym_sh@ushareit.com>
2023-09-19 15:14:10 +0800

    feat: reactive stop function

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,5 +1,9 @@
+import { extend } from "../shared";
 class ReactiveEffect {
   private _fn: any;
+  deps = [];
+  active = true;
+  onStop?: () => void;
   public scheduler: Function | undefined;
   constructor(fn1, scheduler?: Function) {
     this._fn = fn1;
@@ -9,9 +13,25 @@ class ReactiveEffect {
     activeEffect = this;
     return this._fn();
   }
-}
-const targetMap = new Map();
 
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      if(this.onStop) {
+        this.onStop();
+      }
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep: any) => {
+    dep.delete(effect);
+  });
+}
+
+const targetMap = new Map();
 export function track(target, key) {
   // target->key->dep
   let depsMap = targetMap.get(target);
@@ -25,13 +45,16 @@ export function track(target, key) {
     dep = new Set();
     depsMap.set(key, dep);
   }
+  if (!activeEffect) {
+    return;
+  }
   dep.add(activeEffect);
+  activeEffect.deps.push(dep);
 }
 
 export function trigger(target, key) {
   const depsMap = targetMap.get(target);
   const dep = depsMap.get(key);
-
   for (const effect of dep) {
     if (effect.scheduler) {
       effect.scheduler();
@@ -44,8 +67,16 @@ let activeEffect;
 
 export function effect(fn, options: any = {}) {
   const _effect = new ReactiveEffect(fn, options.scheduler);
+  extend(_effect, options);
   _effect.run();
-  return _effect.run.bind(_effect);
+  const runner:any = _effect.run.bind(_effect)
+  runner.effect = _effect;
+
+  return runner;
+}
+
+export function stop(runner) {
+  runner.effect.stop();
 }
 
 
