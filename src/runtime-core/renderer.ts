542b9cfd3cc1dd9fb3675e228a2c5c748acfb590
chenym <chenym_sh@ushareit.com>
2023-10-01 15:27:25 +0800

    feat: shape flag

--- src/runtime-core/renderer.ts
+++ src/runtime-core/renderer.ts
@@ -1,4 +1,5 @@
 import { isObject } from "../shared/src/index";
+import { ShapeFlags } from "../shared/src/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
 import { PublicInstanceProxyHandlers } from "./componentPublicInstance";
 
@@ -11,9 +12,10 @@ function patch(vnode, container) {
   // 是 element 那么就应该处理 element
   // 思考题： 如何去区分是 element 还是 component 类型呢？
   // processElement();
-  if (typeof vnode.type === "string") {
+  const { shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.ELEMENT) {
     processElement(vnode, container);
-  } else if (isObject(vnode.type)) {
+  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
     processComponent(vnode, container);
   }
   console.log(vnode.type);
@@ -25,10 +27,10 @@ function processElement(vnode, container) {
 
 function mountElement(vnode, container) {
   const el = (vnode.el = document.createElement(vnode.type));
-  const { children } = vnode;
-  if (typeof children === 'string') {
+  const { children, shapeFlag } = vnode;
+  if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
-  } else if(Array.isArray(children)) {
+  } else if(shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
     mountChildren(vnode, el);
   }
 
--- src/runtime-core/vnode.ts
+++ src/runtime-core/vnode.ts
@@ -1,10 +1,27 @@
+import { ShapeFlags } from "../shared/src/ShapeFlags";
+
 export function createVNode(type, props?, children?) {
   const vnode = {
     type,
     props,
     children,
-    el:null
+    el:null,
+    shapeFlag: getShapeFlag(type)
   };
 
+  if (typeof children === "string") {
+    vnode.shapeFlag |= ShapeFlags.TEXT_CHILDREN;
+  } else if (Array.isArray(children)) {
+    vnode.shapeFlag |= ShapeFlags.ARRAY_CHILDREN;
+  }
+
   return vnode;
+}
+
+
+
+function getShapeFlag(type) {
+  return typeof type === "string"
+    ? ShapeFlags.ELEMENT
+    : ShapeFlags.STATEFUL_COMPONENT;
 }
